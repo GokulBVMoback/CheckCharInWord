57ee09bad76e66c66fb0137a716743d735dd5f64
GokulBVMoback <113531158+GokulBVMoback@users.noreply.github.com>
2022-09-28 10:33:23 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckCharInWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word;
+            Console.Write("Enter the word:");
+            word = Console.ReadLine();
+            if(word.Contains("G"))
+            {
+                Console.WriteLine("Given word "+word+ " contains G");
+            }
+            else 
+            {
+                Console.WriteLine("Given Word "+word+" not contains G");
+            }
+        }
+    }
+}
